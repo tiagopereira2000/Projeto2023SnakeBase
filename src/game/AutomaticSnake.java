dc89fc27054a0f8585722427cc1ff35c7fc026ff
tiagopereira2000 <tgpaa@iscte-iul.pt>
2023-11-11 15:42:02 +0000

    changes: Snake.eat, AutomaticSnake.run, AutomaticSnake.getNextPosition, AutomaticSnake.resetMove, Snake.move, Snake.digestion

--- src/game/AutomaticSnake.java
+++ src/game/AutomaticSnake.java
@@ -12,6 +12,10 @@ import environment.Cell;
 import environment.Board;
 import environment.BoardPosition;
 
+/**
+ *
+ * @author ASUS-Tiago
+ * */
 public class AutomaticSnake extends Snake {
 	protected boolean resetMove = false; //se a snake tiver sido interrompida para adotar outro caminho
 	public AutomaticSnake(int id, LocalBoard board) {
@@ -26,12 +30,16 @@ public class AutomaticSnake extends Snake {
 
 		//TODO: automatic movement
 		while(true){
-			// buscar ultima posição das celulas para realizar movimento
-			Cell nextCell = getBoard().getCell(getNextPosition());
+			Cell nextCell = getBoard().getCell(getNextPosition()); //NextPosition() -> (BP) bestoption -> (Cell) bestoption
 			try {
-				move(nextCell);
+				move(nextCell); // Snake.move()
 				Thread.sleep(Board.PLAYER_PLAY_INTERVAL);
 			}catch (InterruptedException e){
+				/*
+				* 2 interrupt types:
+				* 	- terminate game() -> Board.isFinished = true -> break: out of while
+				* 	- reset snake move() -> resetMove = true -> continue on while
+				* */
 				if(getBoard().isFinished()){
 					break;
 				}
@@ -41,19 +49,27 @@ public class AutomaticSnake extends Snake {
 		}
 	}
 
+	/**
+	 * getNextPosition:
+	 * @return	best option to move ({@link BoardPosition})
+	 *
+	 * options =
+	 * 	if not Interrupted : { neighbors and (cells not ocupied by the snake) } (1)
+	 * 	if Interrupted : { (1) and (cells not ocupied by obstacles or other snakes) } (2)
+	 * Then, from the options left lets take the nearest to the goal (3)
+	 **/
 	private BoardPosition getNextPosition(){
-		// fazer movimentação para o premio
 		BoardPosition goal = getBoard().getGoalPosition();
 		List<BoardPosition> options = getBoard().getNeighboringPositions(cells.getLast());
 		if(resetMove){ 	//snake interrupted -> excluir células com obstaculos
-			options.removeIf(p -> getBoard().getCell(p).isOcupied());
+			options.removeIf(p -> getBoard().getCell(p).isOcupied()); // (2)
 			resetMove = false;
 		}
-		options.removeIf(i -> getPath().contains(i)); // options - getPath()
-		double min = 30;
-		BoardPosition bestOption = options.get(0); //inicializar com a primeira opção
+		options.removeIf(i -> getPath().contains(i)); //(1)
+		double min = 100; // ~= infinite
+		BoardPosition bestOption = options.get(0); //initialize bestOption = options[0]
 		for (BoardPosition p: options) {
-			double distance = p.distanceTo(goal);
+			double distance = p.distanceTo(goal); // (3)
 			if(distance < min){
 				min = distance;
 				bestOption = p;
