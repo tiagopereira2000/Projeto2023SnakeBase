088ad7e81e048fd7f5eca2a326a0890bf2204252
Gonçalo Lopes <gfcls@iscte-iul.pt>
2023-12-02 17:47:45 +0000

    Inicio do des. do sv

--- src/remote/Client.java
+++ src/remote/Client.java
@@ -1,6 +1,20 @@
 package remote;
 
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
 /** Remore client, only for part II
  * 
  * @author luismota
@@ -10,8 +24,45 @@ package remote;
 public class Client {
 	RemoteBoard myBoard;
 
+	private ObjectInputStream in;
+	private ObjectOutputStream out;
+	private Socket socket;
+
 	public Client(RemoteBoard remoteBoard) {
 		myBoard = remoteBoard;
+	}
+
+	public void runClient() {
+		try {
+			connectToServer();
+			sendIntructions();
+		} catch (IOException e) {// ERRO...
+		} finally {//a fechar...
+			try {
+				socket.close();
+			} catch (IOException e) {//...
+			}
+		}
+	}
+
+
+	void connectToServer() throws IOException {
+			InetAddress endereco = InetAddress.getByName(null);
+			System.out.println("Endereco:" + endereco);
+			socket = new Socket(endereco, Server.PORTO);
+			System.out.println("Socket:" + socket);
+			System.out.println("teste em connect to server <1>");
+			out = new ObjectOutputStream(socket.getOutputStream());
+			in = new ObjectInputStream(socket.getInputStream());
+			System.out.println("teste em connect to server <2>");
+
+
+			System.out.println("teste em connect to server <3>");
+
+	}
+
+	void sendIntructions(){
+
 	}
 
 	public static void main(String[] args) {
@@ -19,6 +70,7 @@ public class Client {
 		// TODO criará uma RemoteBoard para receber a janela do servidor
 		// TODO criar objeto SnakeGui e abrir janela do jogo
 		// TODO vai fazer um pedido de ligação com o Server --> Criar Socket para receber estado do jogo (Board)
+		runClient();
 		// TODO começa a ler o estado do jogo
 	}
 
--- src/remote/Server.java
+++ src/remote/Server.java
@@ -1,28 +1,138 @@
 package remote;
 
 import environment.LocalBoard;
+import game.HumanSnake;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
     private LocalBoard gameState;
+
+    public static final int PORTO = 8080;
+
 
     public Server(LocalBoard gameState) {
         this.gameState = gameState;
     }
 
 
+    public class ClientHandler extends Thread{
+        private ObjectInputStream in;
+        private ObjectOutputStream out;
+        Socket cliente;
 
-    public void startServing() throws IOException{
-        //TODO
-    }
-    void doConnections() throws IOException{
-        //TODO
+        int count;  //Contagem dos IDS das snakes
+
+        HumanSnake humanSnake; //cada client handler tem uma snake associada que vai ser o objeto do jogo de cada player
+
+        ClientHandler(Socket socket){
+            this.cliente = socket;
+        }
+
+        public void run(){
+            try{
+                doConnections(cliente);
+                initializeSnake();
+                serve();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+
+        }
+
+        void doConnections(Socket socket) throws IOException{
+            try{
+                in = new ObjectInputStream(socket.getInputStream());
+                out = new ObjectOutputStream(socket.getOutputStream());
+
+
+
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+
+        }
+
+        void initializeSnake(){
+            count++; // itera o contador de ids de snakes
+
+            humanSnake = new HumanSnake(count,gameState);
+        }
+
+        private void serve() throws IOException{
+            try {
+
+
+                while (in.readObject() != null) {
+                    //Aqui recebe as intruçoes do jogador e executa o necessario para que o jogo seja atualizado de maneira certa
+
+
+                    //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+                    //Envia a Board
+                    out.writeObject(gameState);
+                    out.flush();
+                }
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            finally {
+                try {
+                    if (out != null) {
+                        out.close();
+                    }
+                    if (in != null) {
+                        in.close();
+                        cliente.close();
+                    }
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        }
+
+
+
+
     }
 
-    private void serve() throws IOException{
-        //TODO
+
+    public void startServing() throws IOException {
+        ServerSocket ss = new ServerSocket(PORTO);
+
+        try {
+            while (true) {
+                Socket socket = ss.accept();
+
+
+                // Aceita a conecção
+                ClientHandler clienteSock = new ClientHandler(socket);
+                clienteSock.start();
+
+            }
+        } finally {
+            // Fecha o socket
+            ss.close();
+        }
     }
+
+
+
+
+
 
     /**
      *  começará o jogo LocalBoard.init()
